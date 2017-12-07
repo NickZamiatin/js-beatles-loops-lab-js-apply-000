9963b6cb380a07ed314c4a313803ad4d9f2de56d
MYKYTA ZAMIATIN <4406015@gmail.com>
2017-12-07 21:29:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function theBeatlesPlay(musicians, instruments) {
+  const arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr
+}
+
+function johnLennonFacts(facts) {
+  const Facts = []
+
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return Facts
+}
